1230f5334ce9e12a2c32007bc9bb4953d7661bdb
Markkelvin14 <mjkelvin38@gmail.com>
2023-05-30 18:13:27 +0000

    Python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class square:
+    """show a square"""
+
+    def __init__(self, size):
+        """initialize the square"""
+
+        self.__size = size
