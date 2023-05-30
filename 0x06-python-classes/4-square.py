fcb29c0a12342a4d1fec4c8cfd44544c738e5d52
Markkelvin14 <mjkelvin38@gmail.com>
2023-05-30 20:04:53 +0000

    Python classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,8 +6,8 @@ class Square:
     """show a square"""
 
     def __init__(self, size=0):
-         """Initialize a new square"""
-         self.size = size
+        """Initialize a new square"""
+        self.size = size
 
     @property
     def size(self):
