95fa37f5f187599f874b4269782d304134770b8f
Markkelvin14 <mjkelvin38@gmail.com>
2023-06-12 13:55:25 +0000

    Python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define a BaseGeometry"""
+
+
+class BaseGeometry:
+    """create a clase"""
+
+    def area(self):
+        """define a method"""
+        raise ValueError("area() is not implemented")
