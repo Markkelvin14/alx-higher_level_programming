9866b195257d614932d9bbfeab2d694c2c2cf69d
Markkelvin14 <mjkelvin38@gmail.com>
2023-06-12 16:01:28 +0000

    Python inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,9 +9,9 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Intialize a new Rectangle"""
 
-        self.integer_validator("width", width)
+        super.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super.integer_validator("height", height)
         self.__height = height
 
     def area():
