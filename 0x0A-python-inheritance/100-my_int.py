95dea06e0048347ca7bcc7df673a627343be70ab
Markkelvin14 <mjkelvin38@gmail.com>
2023-06-12 16:59:11 +0000

    Python inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,8 +7,8 @@ class MyInt(int):
 
     def __eq__(self, value):
         """rewrite == opeartor with != behavior."""
-        return self.ans != value
+        return self.real != value
 
     def __ne__(self, value):
         """rewrite != operator with == behavior."""
-        return self.ans == value
+        return self.real == value
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,9 +2,9 @@
 """Defines a function that adds attributes to objects."""
 
 
-def add_att(obj, atri, value):
-        """Add a new attribute to an object if possible."""
+def add_atitribute(obj, atri, value):
+    """Add a new attribute to an object if possible."""
 
-        if not hasattr(obj, "__dict__"):
-            raise TypeError("can't add new attribute")
-        setattr(obj, atri, value)
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, atri, value)
