e49cc916cefd8ca7a951cf9f20cea8a0145c302e
Markkelvin14 <mjkelvin38@gmail.com>
2023-06-13 21:02:48 +0000

    Python input output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,10 +6,10 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-         """Initialize a new Student."""
-         self.first_name = first_name
-         self.last_name = last_name
-         self.age = age
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """return a dictionary rep of a student"""
