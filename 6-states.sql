084f1aac3c6b0f99ef0dfae38797a95b7a02498b
Markkelvin14 <122996163+Markkelvin14@users.noreply.github.com>
2023-07-20 10:36:26 -0800

    Delete 6-states.sql

--- 6-states.sql
+++
@@ -1,6 +0,0 @@
--- creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- use database
-USE hbtn_0d_usa;
--- create table
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
