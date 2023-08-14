7334e42526086ded462c24a3d4a5dfe5e057fc66
Markkelvin14 <mjkelvin38@gmail.com>
2023-08-14 18:02:10 +0000

    JavaScript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-/*
- command line arguments using process.argv
- */
-if (process.argv.length < 3) {
+if (process.argv.length <= 2) {
 	console.log('No argument');
-} else if (process.argv.length > 3 ) {
+} else if (process.argv.length > 2 ) {
 	console.log('Arguments found');
 } else {
-	console.log('Arguments');
+	console.log('Argument found');
 }
