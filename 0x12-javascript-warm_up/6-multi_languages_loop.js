34fe49dc409d75fd25233f2edf503fdd72bfaa40
Markkelvin14 <mjkelvin38@gmail.com>
2023-08-14 20:12:26 +0000

    JavaScript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'JavaSript is amazing'];
-for (const item of list) {
+const lang = ['C is fun', 'Python is cool', 'JavaSript is amazing'];
+for (const item of lang) {
   console.log(item);
 }
