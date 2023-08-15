a8311fceedc18884b17456bed8a53df8f49775f7
Markkelvin14 <mjkelvin38@gmail.com>
2023-08-15 09:31:29 +0000

    JavaScript

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let Num = parseInt(process.argv[2]);
+function factorial (Num) {
+  if (Num === 1 || isNaN(Num)) {
+    return (1);
+  } else {
+    return (Num * factorial(Num - 1));
+  }
+}
+console.log(factorial(Num));
