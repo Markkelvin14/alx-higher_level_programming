d65f54b5d2ea45d4e3257246fb25e6c2438d36ce
Markkelvin14 <mjkelvin38@gmail.com>
2023-09-01 18:30:40 +0000

    python-network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = argv[1]
     data = {'email': argv[2]}
     data = parse.urlencode(data)
-    data = data.encode('ascii')
+    data = data.encode('utf8')
     req = request.Request(url, data)
     with utllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
